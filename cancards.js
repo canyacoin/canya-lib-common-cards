51d3ba994d97c8800c9d74cde551a949bfa2ff6f
wie-canya <38897356+wie-canya@users.noreply.github.com>
2018-06-27 16:26:28 +1000

    A more streamlined method for CanApp Card creation

--- cancards.js
+++ cancards.js
@@ -1,86 +1,97 @@
 // the array containing ALL CanApps. will be updated periodically.  
-var canApps = [
+var canAppArray = [
     {
-        name            :   'CanInvoice',
-        illustration    :   'https://canya.io/images/CanApps/illustrations/CanInvoice.svg',
-        logo            :   'https://canya.io/images/CanApps/logos/CanInvoice.svg',
-        desc            :   'Easily create your own clean, professional and accurate invoices.',
+        name            :   'CanInvoice',  
+        desc            :   'Easily create your own clean, professional and accurate invoices',
         url             :   'https://caninvoice.io'
     },
     {
-        name            :   'CanStation',
-        illustration    :   'https://canya.io/images/CanApps/illustrations/CanStation.svg',
-        logo            :   'https://canya.io/images/CanApps/logos/CanStation.svg',
-        desc            :   'Find out the ideal amount of gas to allocate to your ETH transaction!',
+        name            :   'CanStation', 
+        desc            :   'Find out the ideal amount of gas to allocate to your ETH transaction',
         url             :   'https://CanStation.io'
     },
     {
-        name            :   'CanYa25',
-        illustration    :   'https://canya.io/images/CanApps/illustrations/CanYa25.svg',
-        logo            :   'https://canya.io/images/CanApps/logos/CanYa25.svg',
-        desc            :   'Shoot your pitch, vote, contribute and raise funds!',
-        url             :   'https://CanYa25.io'
+        name            :   'CanFund', 
+        desc            :   'Shoot your pitch, vote, contribute and raise funds',
+        url             :   'https://CanFund.io'
     },
     {
-        name            :   'CanSend',
-        illustration    :   'https://canya.io/images/CanApps/illustrations/CanSend.svg',
-        logo            :   'https://canya.io/images/CanApps/logos/CanSend.svg',
-        desc            :   'Send ERC20 tokens to multiple addresses at once!',
+        name            :   'CanSend', 
+        desc            :   'Send ERC20 tokens to multiple addresses at once',
         url             :   'https://CanSend.io'
     },
     {
-        name            :   'CanShare',
-        illustration    :   'https://canya.io/images/CanApps/illustrations/CanShare.svg',
-        logo            :   'https://canya.io/images/CanApps/logos/CanShare.svg',
-        desc            :   'Easily create your own clean, professional and accurate invoices.',
+        name            :   'CanShare', 
+        desc            :   'Send and receive files using distributed technology',
         url             :   'https://canshare.io/'
     },
     {
-        name            :   'CanTrack',
-        illustration    :   'https://canya.io/images/CanApps/illustrations/CanTrack.svg',
-        logo            :   'https://canya.io/images/CanApps/logos/CanTrack.svg',
-        desc            :   'A crisp and easy to use task tracker to help you with your project.',
+        name            :   'CanTrack', 
+        desc            :   'A crisp and easy to use task tracker to help you with your project',
         url             :   'https://CanTrack.io'
-    } 
-]; 
-
+    },
+    {
+        name            :   'CanSign', 
+        desc            :   'Upload documents to IPFS and sign them with your Ethereum address.',
+        url             :   'https://CanSign.io'
+    },
+    {
+        name            :   'CanSeek', 
+        desc            :   'Hire and be hired with decentralised technology',
+        url             :   'https://CanSeek.io'
+    }
+];  
 //Creates the cards in a random order, excluding the current app's card.
 function createCardsRandomly(maxCards, thisApp){   
 	   var prevIndexes = Array(); 
 		var min = 0;
-		var max = canApps.length-1;
+		var max = canAppArray.length-1;
 		//create random number 
         var createdCards = 0; 
         while(createdCards < maxCards){ //loop through the logic until we created the target number of cards.
             var randNumber  = Math.floor(Math.random() * (max - min + 1)) + min; // Random number generator
-            if(thisApp != canApps[randNumber].name){  // if the c
-                console.log("randomNumber= "+randNumber);
-                console.log((prevIndexes.includes(randNumber)));
-                if((prevIndexes.includes(randNumber))==true){ 
-                    console.log("randomNumber is in the prevIndexes, getting new number...");    
-                    var randNumber  = Math.floor(Math.random() * (max - min + 1)) + min; 
+            if(thisApp != canAppArray[randNumber].name){  // if the c
+                 if((prevIndexes.includes(randNumber))==true){ 
+                     var randNumber  = Math.floor(Math.random() * (max - min + 1)) + min; 
                 }
                 else{
-                            createCard(canApps[randNumber]);   
+                            createCard(canAppArray[randNumber]);   
                             createdCards++;
                             prevIndexes.push(randNumber);
                 }
             } 
         } 
         prevIndexes.push(randNumber);
-    } 
-//Creates ALL the cards, excluding the current app's card. thisApp blank if you want to show EVERYTHING.
+    }  
+//Creates 6  cards, excluding the current app's card. thisApp blank if you want to show EVERYTHING. 
 function createCards(thisApp){    
     //console.log(getJSON('http://canya.io/assets/cancards.json')); 
     //createCard("canInvoice"); 
-    console.log(typeof canApps);
-    for (var i = 0 ; i<canApps.length ; i++){  
-        if(thisApp != canApps[i].name){
-            createCard(canApps[i]); 
+    var shuffled = shuffle(canAppArray);
+    console.log(typeof canAppArray); 
+    for (var i = 0 ; i<6 ; i++){  
+        if(thisApp != canAppArray[i].name){
+            createCard(shuffled[i]); 
         }  
     } 
  } 
- 
+function shuffle(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  } 
+  return array;
+} 
 //the actual card factory.
 function createCard(canApp){ 
 	/* create the card container
@@ -96,6 +107,7 @@ function createCard(canApp){
 					</div>
 				</a> 
 	*/
+    console.log(canApp);
      var card = document.createElement('a');
      card.href = canApp.url;
      card.target = "_blank";
@@ -105,11 +117,11 @@ function createCard(canApp){
      card.className = "canAppCard noDecor"; 
      
      var cardIllustration = document.createElement('img'); 
-     cardIllustration.src = canApp.illustration;
+     cardIllustration.src = "https://canya.io/images/CanApps/illustrations/"+canApp.name+".svg";
      cardIllustration.className = "cardIllustration";  
      
      var cardLogo = document.createElement('img'); 
-     cardLogo.src = canApp.logo;
+     cardLogo.src = "https://canya.io/images/CanApps/logos/"+canApp.name+".svg";
      cardLogo.className = "cardLogo"; 
      
      var cardText = document.createElement('p');
@@ -118,7 +130,7 @@ function createCard(canApp){
      
      var cardButton = document.createElement('a');
      cardButton.className = "cardBtn noDecor";
-     cardButton.innerHTML = "Check It Out!";  
+     cardButton.innerHTML = "Check It Out";  
     
     
      cardDiv.appendChild(cardIllustration);
